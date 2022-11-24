0d1d23de4d35f370b3a7a9e327bd633ee41b4377
DELL <DELL@Vivek>
2022-11-24 19:44:54 +0530

    comitteezzzz

---
+++ src/test/java/Earth/Z_Warriors.java
@@ -0,0 +1,20 @@
+package Earth;
+
+import org.testng.annotations.Test;
+
+public class Z_Warriors {
+	
+	@Test
+	public void Freeza() {
+		
+		System.out.println("Freeza");
+	}
+	
+	@Test
+	public void Destruction() {
+		
+		System.out.println("Beerus");
+		
+	}
+
+}
